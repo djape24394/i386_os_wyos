78d38c7c8f57093ecc0534dcf2bff869c004c18b
Predrag Vasilic <pedjav94@gmail.com>
2021-05-14 22:22:02 +0200

    Updated printf function to support printing on 25x80 screen

--- myos/kernel.cpp
+++ myos/kernel.cpp
@@ -3,11 +3,36 @@
 
 void printf(char* str)
 {
+    // TODO: refactor this later, no magic numbers
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-
+    // y is a row, x is a column, the screen size is 25x80
+    static uint8_t x = 0U, y = 0U;
     for(int i = 0; str[i] != '\0'; ++i)
     {
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        if(str[i] == '\n')
+        {
+            x = 0U;
+            y++;
+        }else
+        {
+            // every other byte is filled, that why we use this uint16_t
+            VideoMemory[y * 80 + x] = (VideoMemory[y * 80 + x] & 0xFF00) | str[i];
+            x++;
+        }
+
+        if(x >= 80)
+        {
+            y++;
+            x = 0U;
+        }
+
+        if(y >= 25)
+        {
+            for(int i = 0; i < 25; i++)
+                for(int j = 0; j < 80; j++) VideoMemory[i * 80 + j] = (VideoMemory[i * 80 + j] & 0xFF00) | ' ';
+            x = 0U;
+            y = 0U;
+        } 
     }
 }
 
@@ -24,7 +49,8 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnum)
 {
-    printf("Hello World!");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
 
     GlobalDescriptorTable gdt;
 
